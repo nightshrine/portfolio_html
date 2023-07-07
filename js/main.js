5e15ee476264a92dac70facc3ba139d97f14996f
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2023-07-07 19:59:05 +0900

    もう一回make contact-form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+let isScroll = false;
+let myVideo = document.querySelector("#myVideo");
+myVideo.style.opacity = 1;
+
 window.onload = function () {
     setTimeout(moveToWorks, 3000); // 3秒後にmoveToDiv関数を実行
 };
 
+window.addEventListener("scroll", () => {
+    isScroll = true;
+    let scrollHeight = document.documentElement.scrollTop;
+    let videoOpacity = Math.max(1200 - scrollHeight, 200) / 1000
+    myVideo.style.opacity = videoOpacity;
+})
+
 function moveToWorks() {
+    if(isScroll) {
+        return;
+    }
     var worksDiv = document.querySelector("#about");
     worksDiv.scrollIntoView({ behavior: "smooth" }); // ページをスクロールしてworksDivが表示されるようにする
 }
